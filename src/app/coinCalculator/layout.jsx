ce31a3116e9895a71833a33a40183cb107361b17
ycarruido <yhonatandcarruido@gmail.com>
2024-12-09 23:10:02 -0400

    Antes de Three.Js

--- src/app/coinCalculator/layout.jsx
+++ src/app/coinCalculator/layout.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 export const metadata = {
-  title: "DACSystems - Calculadora de Criptomonedas",
+  title: "DACSoft - Calculadora de Criptomonedas",
   description: "Calcula el valor de las principales criptomonedas en diferentes monedas fiat de forma rápida y sencilla con nuestra calculadora de criptomonedas.",
   keywords: "calculadora de criptomonedas, Bitcoin, Ethereum, Litecoin, monedas fiat, USD, EUR, VES",
 };
